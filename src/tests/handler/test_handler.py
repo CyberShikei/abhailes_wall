99c2273d613f4c1b708227fa36c8ded5decef226
CyberShikei <jacopferreira@gmail.com>
2025-01-17 19:24:47 +0200

    Update ignore

--- src/tests/handler/test_handler.py
+++ src/tests/handler/test_handler.py
@@ -4,7 +4,7 @@ import os
 from src.handler import download_image
 
 TEST_IMAGE = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-IMAGE_PATH = "public/images/image_that_should_not_exist.jpg"
+IMAGE_PATH = "public/test_images/image_that_should_not_exist.jpg"
 
 
 class TestHandler():
